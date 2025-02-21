b975bd7766d48ce9dce80c71f42a1a6e9f203561
Fabienholert <fabienholert@gmail.com>
2025-02-21 19:38:46 +0100

    Ajout de la middleware d'authentification pour sécuriser les routes de gestion des livres

--- routes/book.js
+++ routes/book.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const auth = require("../middleware/auth.js");
 const router = express.Router();
 
 const bookCtrl = require("../controllers/book.js");
 
-router.post("/", bookCtrl.createBook);
-router.put("/:id", bookCtrl.modifyBook);
-router.delete("/:id", bookCtrl.deleteBook);
+router.post("/", auth, bookCtrl.createBook);
+router.put("/:id", auth, bookCtrl.modifyBook);
+router.delete("/:id", auth, bookCtrl.deleteBook);
 router.get("/:id", bookCtrl.getOneBook);
 router.get("/", bookCtrl.getAllBooks);
 
