363464230027495e109f4a2e7d685f1dfd944ad2
Fabienholert <fabienholert@gmail.com>
2025-02-21 23:16:54 +0100

    Correction de la suppression de l'identifiant utilisateur lors de la création d'un livre

--- controllers/book.js
+++ controllers/book.js
@@ -4,10 +4,10 @@ const fs = require("fs");
 exports.createBook = (req, res, next) => {
   const bookObject = JSON.parse(req.body.book);
   delete bookObject._id;
-  delete bookObject.userId;
+  delete bookObject._userId;
   const book = new Book({
     ...bookObject,
-    userId: req.auth.use,
+    userId: req.auth.userId,
     imageUrl: `${req.protocol}://${req.get("host")}/images/${
       req.file.filename
     }`,
